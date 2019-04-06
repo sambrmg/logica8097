c1e9f059992cf94d1b538fdb07d42f60dfd35e5b
Guilherme <sambrmg@gmail.com>
2019-04-06 15:48:15 -0300

    tabela

---
+++ dom.js
@@ -0,0 +1,35 @@
+
+
+
+var lista = []
+
+function salvarAluno(){
+    var nome = document.querySelector("#nome").value;
+    var nota = document.querySelector("#nota").value;
+    if(nome != "" && nota != ""){
+        lista.push(
+            {
+               nomeAluno: nome,
+               notaAluno: parseFloat(nota)
+            }
+        )
+    
+        document.querySelector("#nome").value = ""
+        document.querySelector("#nota").value = ""
+        alert("aluno: " + nome + " inserido com sucesso")
+        montarTabela()
+    }else{
+        alert("informe um nome e uma nota")
+    }
+}
+
+function montarTabela(){
+    var tbody = "";
+    var total = 0;
+    for(var i = 0; i < lista.length; i++ ){
+        total = total + lista[i].notaAluno;
+        tbody =  "<tr><td>"+ lista[i].nomeAluno +"</td><td>"+ lista[i].notaAluno +"</td></tr>" +tbody
+    }
+    document.getElementById("linhaTabela").innerHTML = tbody
+    document.getElementById("linhaTotal").innerHTML = "<tr><td>Total</td><td>"+ total +"</td></tr>";
+}
